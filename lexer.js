3dd84ba10731fa7ccb35f8e24f3198ad4765759e
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2024-08-26 16:39:10 -0700

    tokenizer working

--- lexer.js
+++ lexer.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.TokenType = void 0;
+exports.Token = exports.TokenType = void 0;
 var TokenType;
 (function (TokenType) {
     TokenType[TokenType["Number"] = 0] = "Number";
@@ -9,12 +9,67 @@ var TokenType;
     TokenType[TokenType["OpenParen"] = 3] = "OpenParen";
     TokenType[TokenType["CloseParen"] = 4] = "CloseParen";
     TokenType[TokenType["BinaryOperator"] = 5] = "BinaryOperator";
-    TokenType[TokenType["Assign"] = 6] = "Assign";
+    TokenType[TokenType["Let"] = 6] = "Let";
 })(TokenType || (exports.TokenType = TokenType = {}));
+var KEYWORDS = {
+    "let": TokenType.Let,
+};
+var Token = /** @class */ (function () {
+    function Token(value, type) {
+        this.value = value;
+        this.type = type;
+    }
+    return Token;
+}());
+exports.Token = Token;
+function isAlpha(char) {
+    return char.toLowerCase() !== char.toUpperCase();
+}
+function isNumber(char) {
+    return !(isNaN(parseInt(char)));
+}
 function tokenize(sourceCode) {
     var tokens = [];
     var src = sourceCode.split("");
     console.log(src);
+    while (src.length > 0) {
+        if (src[0] === '(') {
+            tokens.push(new Token(src.shift(), TokenType.OpenParen));
+        }
+        else if (src[0] === ')') {
+            tokens.push(new Token(src.shift(), TokenType.CloseParen));
+        }
+        else if (src[0] === '+' || src[0] === '-' || src[0] === '*' || src[0] === '/') {
+            tokens.push(new Token(src.shift(), TokenType.BinaryOperator));
+        }
+        else if (src[0] === '=') {
+            tokens.push(new Token(src.shift(), TokenType.Equals));
+        }
+        else {
+            if (isAlpha(src[0])) {
+                var str = "";
+                while (src.length > 0 && isAlpha(src[0])) {
+                    str += src.shift();
+                }
+                var reserved = KEYWORDS[str];
+                console.log(reserved);
+                if (reserved === undefined) {
+                    tokens.push(new Token(str, TokenType.Identifier));
+                }
+                else {
+                    tokens.push(new Token(str, reserved));
+                }
+            }
+            else if (isNumber(src[0])) {
+                var num = "";
+                while (src.length > 0 && isNumber(src[0])) {
+                    num += src.shift();
+                }
+                tokens.push(new Token(num, TokenType.Number));
+            }
+            src.shift();
+        }
+    }
     return tokens;
 }
-tokenize("haha");
+console.log(tokenize("let haha = 0 + (23 / 2)"));
--- lexer.ts
+++ lexer.ts
@@ -5,23 +5,76 @@ export enum TokenType {
   OpenParen,
   CloseParen,
   BinaryOperator,
-  Assign
+  Let
 }
 
-export interface Token {
+const KEYWORDS: Record<string, TokenType> = {
+  "let": TokenType.Let,
+}
+
+export class Token {
+
   value: string;
   type: TokenType;
+
+  constructor(value: string, type: TokenType) {
+    this.value = value;
+    this.type = type;
+  }
 }
 
-function tokenize(sourceCode: string) {
+function isAlpha(char: string): boolean {
+  return char.toLowerCase() !== char.toUpperCase();
+}
+
+function isNumber(char: string): boolean {
+  return !(isNaN(parseInt(char)));
+}
+
+function tokenize(sourceCode: string): Token[] {
 
   const tokens: Token[] = [];
   const src: string[] = sourceCode.split("");
 
   console.log(src);
+
+  while (src.length > 0) {
+    if (src[0] === '(') {
+      tokens.push(new Token(src.shift()!, TokenType.OpenParen));
+    } else if (src[0] === ')') {
+      tokens.push(new Token(src.shift()!, TokenType.CloseParen));
+    } else if (src[0] === '+' || src[0] === '-' || src[0] === '*' || src[0] === '/') {
+      tokens.push(new Token(src.shift()!, TokenType.BinaryOperator));
+    } else if (src[0] === '=') {
+      tokens.push(new Token(src.shift()!, TokenType.Equals));
+    } else {
+      if (isAlpha(src[0])) {
+        let str = "";
+        while (src.length > 0 && isAlpha(src[0])) {
+          str += src.shift();
+        }
+
+        const reserved = KEYWORDS[str];
+        console.log(reserved);
+        if (reserved === undefined) {
+          tokens.push(new Token(str, TokenType.Identifier));
+        } else {
+          tokens.push(new Token(str, reserved));
+        }
+
+      } else if (isNumber(src[0])) {
+        let num = "";
+        while (src.length > 0 && isNumber(src[0])) {
+          num += src.shift();
+        }
+        tokens.push(new Token(num, TokenType.Number));
+      }
+      src.shift();
+    }
+  }
   
   return tokens;
 
 }
 
-tokenize("haha");
+console.log(tokenize("let haha = 0 + (23 / 2)"));
